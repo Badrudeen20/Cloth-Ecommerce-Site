cecb924ca82acc681333b7a076008e7ff282cccd
badrudeen ansari <54722228+Badrudeen20@users.noreply.github.com>
2021-10-03 20:36:28 +0530

    Update Customer.php

--- Admin/Customer.php
+++ Admin/Customer.php
@@ -13,28 +13,24 @@
                     <thead>
                         <tr>
                             <th>S.No</th>
-                            <th>Profile</th>
                             <th>Name</th>
                             <th>Email</th>
                             <th>Phone</th>
-                            <th>Added On</th>
                             
                         </tr>
                     </thead>
                     <tbody>
                         <?php
-                         $cus_res = mysqli_query($conn,"select * from customer");
+                         $cus_res = mysqli_query($conn,"select * from users");
                           if(mysqli_num_rows($cus_res) >0){
 
                          $i=1;
                           while($cus_row  = mysqli_fetch_assoc($cus_res)){ ?>
                                  <tr>
                             <td><?php echo $i ?></td>
-                            <td><img src="../Client/Image/Product/shirt.jpeg"/></td>
                             <td><?php echo $cus_row['name'] ?></td>
                             <td><?php echo $cus_row['email'] ?></td>
                             <td><?php echo $cus_row['phone'] ?></td>
-                            <td><?php echo $cus_row['added_on'] ?></td>
                         </tr>
                     
                         <?php $i++; } 
